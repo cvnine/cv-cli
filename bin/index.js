86ee5ee32f7527f2d049c299630bf28fb89a45d1
ihupoo <840499492@qq.com>
2021-01-25 14:21:50 +0800

    更新模板package写入，增加mobile

--- bin/index.js
+++ bin/index.js
@@ -2,30 +2,29 @@
 
 const program = require('commander')
 const chalk = require('chalk')
-const create = require('../lib/create')
 
 program
-    .version(require('../package.json').version)
-    .usage('<command> [options]')
+	.version(require('../package.json').version)
+	.usage('<command> [options]')
 
 program
-    .command('create <app-name>')
-    .description('Create a new React App')
-    .action(name => {
-        console.log(chalk.green(`IH CLI v${require('../package.json').version}`))
-        const create = require('../lib/create');
-        create(name);
-    })
+	.command('create <app-name>')
+	.description('Create a new React App')
+	.action(name => {
+		console.log(chalk.green(`IH CLI v${require('../package.json').version}`))
+		const create = require('../lib/create');
+		create(name);
+	})
 
 program
-    .arguments('<command>')
-    .action(cmd => {
-        console.log(chalk.red(`❌ Unknown command ${chalk.yellow(cmd)}.`));
-        program.outputHelp()
-    })
+	.arguments('<command>')
+	.action(cmd => {
+		console.log(chalk.red(`❌ Unknown command ${chalk.yellow(cmd)}.`));
+		program.outputHelp()
+	})
 
 program.parse(process.argv)
 
-if(!program.args.length){
-    program.outputHelp()
+if (!program.args.length) {
+	program.outputHelp()
 }
